c807a0f66095b240cfddc31d6f2eb6f49589c02e
isabelarivera <ir59@njit.edu>
2021-02-16 23:05:51 -0500

    homework

---
+++ HW6/PHP_Operators.php
@@ -0,0 +1,13 @@
+
+<?php
+echo "<br>\nPHP Exercise [operators] Exercise [1]<br>\n";
+echo 10 * 5;
+echo "<br>\nPHP Exercise [operators] Exercise [2]<br>\n";
+echo 10 / 2;
+echo "<br>\nPHP Exercise [operators] Exercise [3]<br>\n";
+var_dump($a == $b);
+echo "<br>\nPHP Exercise [operators] Exercise [4]<br>\n";
+var_dump($a != $b);
+
+
+?>
